7b7f57ee0819eea262518a8a1bd690b6a0cde3e6
Adam Hulbert <hulbert.adam@gmail.com>
2015-02-08 12:00:43 +1100

    small GLText tidy up

--- source/game_objects/GLText.cpp
+++ source/game_objects/GLText.cpp
@@ -5,15 +5,9 @@
 
 using namespace std;
 
-GLText::GLText()
-{
-	
-}
-
-
-GLText::~GLText(void)
-{
-}
+GLText::GLText(){}
+GLText::~GLText(void){}
+void GLText::Update(float delta_){}
 
 void GLText::SetPos(Vector2 pos_)
 {
@@ -25,13 +19,13 @@ void GLText::SetText(string text_)
 	uvs.clear();
 	UVTranslator trans(1024,1024,64,64);
 
-	for ( int i = 0; i < text_.size(); ++i )
+	for ( int ch : text_ )
 	{
 		//get the ascii representation of the character
-		int ch = (int)toupper(text_[i]);
+		ch = (int)toupper(ch);
 
 		ch -= 32; //sprite chart starts from ascii 32
-		ch -= 5; //accidentally missed 5 chars when creating sheet
+		ch -= 5;  //accidentally missed 5 chars when creating sheet
 		
 		//create an array to pushback into UVS
 		std::array<float, 4> tempArray;
@@ -40,20 +34,21 @@ void GLText::SetText(string text_)
 		int row = ch / 16;
 		int col = ch % 16;
 
-		trans.GetUV(tempArray.data(), row,col);
+		//dump the uv data into the std::array
+		trans.GetUV(tempArray.data(), row, col);
+
+		//uvs is a std::vector<std::array<float, 4>> uvs;
 		uvs.push_back(tempArray);
 	}
 }
 
-void GLText::Update(float delta_)
-{}
-
 void GLText::Draw()
 {
+	//for each std::array in uvs
 	for ( int i = 0; i < uvs.size(); ++i )
 	{
 		SetSpriteUVCoordinates(SpriteSheet::FontSprite(), uvs[i].data());
-		MoveSprite(SpriteSheet::FontSprite(), pos.x + 8 * i, pos.y);
+		MoveSprite(SpriteSheet::FontSprite(), pos.x + 8 * i, pos.y); //hardcoded 8 as a step for now, variable size can come later
 		DrawSprite(SpriteSheet::FontSprite());
 	}
 }
